8aea830c42572bad7f7f8566d97b1e19499c45ae
hung <s7300bka@gmail.com>
2017-08-07 10:48:43 -0700

    Initial

---
+++ data/config/config.php
@@ -0,0 +1,22 @@
+<?php
+define('ECCUBE_INSTALL', 'ON');
+define('HTTP_URL', 'http://localhost/eccube/html/');
+define('HTTPS_URL', 'http://localhost/eccube/html/');
+define('ROOT_URLPATH', '/eccube/html/');
+define('DOMAIN_NAME', '');
+define('DB_TYPE', 'mysql');
+define('DB_USER', 'admin1');
+define('DB_PASSWORD', 'admin1');
+define('DB_SERVER', '127.0.0.1');
+define('DB_NAME', 'eccube');
+define('DB_PORT', '3306');
+define('ADMIN_DIR', 'admin/');
+define('ADMIN_FORCE_SSL', FALSE);
+define('ADMIN_ALLOW_HOSTS', 'a:0:{}');
+define('AUTH_MAGIC', 'bicraephiaslecuchustaiwecraestimoujiopea');
+define('PASSWORD_HASH_ALGOS', 'sha256');
+define('MAIL_BACKEND', 'mail');
+define('SMTP_HOST', '');
+define('SMTP_PORT', '');
+define('SMTP_USER', '');
+define('SMTP_PASSWORD', '');
